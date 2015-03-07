15ad0200b9c60de375cc53f99eff813cb144130f
Julien De Luca <jide.fr@gmail.com>
2015-03-07 16:48:02 +0100

    Animation test

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,15 +8,36 @@ if (typeof ejecta !== 'undefined') {
 };
 
 var Stage = ReactPIXI.Stage;
-
 var Sprite = ReactPIXI.Sprite;
 var Text = ReactPIXI.Text;
 
 var App = React.createClass({
+  _raf: null,
+
+  addRandomSprite: function(event) {
+    console.log(event);
+  },
+
+  getInitialState: function() {
+    return {
+      x: 0
+    };
+  },
+
+  componentDidMount: function() {
+    this.tick();
+  },
+
+  tick: function() {
+    //console.log('tick');
+    this.setState({x: this.state.x + 1});
+    this._tick = requestAnimationFrame(this.tick);
+  },
+
   render: function() {
     var fontstyle = {font:'40px TimesNewRomanPSMT'};
     return <Stage width={this.props.width} height={this.props.height}>
-      <Sprite image={ 'images/raspberry.png' } width={this.props.width} height={this.props.height} />
+      <Sprite x={ this.state.x } interactive={ true } click={ this.addRandomSprite } image={ 'images/raspberry.png' } width={this.props.width} height={this.props.height} />
       <Text text="Vector text" x={10} y={10} style={fontstyle} />
     </Stage>;
   }
