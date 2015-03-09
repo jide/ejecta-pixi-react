5ccf0daedc36467b3580e63f117002a83aaa8f1b
Julien De Luca <jide.fr@gmail.com>
2015-03-09 17:03:43 +0100

    Better logging

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -32,6 +32,24 @@ window.canvas.setAttribute = function(attr, value){
   this[attr] = value;
 };
 
+window.console = {
+  _log: function(level, args) {
+    var txt = level + ':';
+    for (var i = 0; i < args.length; i++) {
+      txt += ' ' + (typeof args[i] !== 'object' ? args[i].toString() : JSON.stringify(args[i]));
+    }
+    ej.log( txt );
+  },
+  
+  assert: function() {
+    var args = Array.prototype.slice.call(arguments);
+    var assertion = args.shift();
+    if( !assertion ) {
+      ej.log( 'Assertion failed: ' + args.join(', ') );
+    }
+  }
+};
+
 EjectaReactShim = function(outerHTML) {
   window.canvas['data-reactid'] = outerHTML.match(/ data-reactid="([^"]*)"/)[1];
   window.canvas['data-react-checksum'] = outerHTML.match(/ data-react-checksum="([^"]*)"/)[1];
