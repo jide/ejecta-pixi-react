2ace226e989c3b03274a5c69413934ac0670929e
Julien De Luca <jide.fr@gmail.com>
2015-03-09 19:48:35 +0100

    Fix events support

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,10 +6,14 @@ var Sprite = ReactPIXI.Sprite;
 var Text = ReactPIXI.Text;
 
 var App = React.createClass({
+  handleClick: function(event) {
+    console.log('click');
+  },
+
   render: function() {
     var fontstyle = {font:'40px Times'};
     return <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Sprite image={'images/raspberry.png'} width={200} height={200} />
+      <Sprite interactive={ true } touchstart={ this.handleClick } image={'images/raspberry.png'} width={200} height={200} />
       <Text text="Vector text" x={0} y={10} style={fontstyle} />
     </Stage>;
   }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,12 +6,14 @@ var DOC_NODE_TYPE = 9;
 document.nodeType = DOC_NODE_TYPE;
 document.body.nodeType = ELEMENT_NODE_TYPE;
 
+// React checks this for IE.
 document.documentElement = {
   style: {
-    cssFloat: 'fake'
+    cssFloat: 'none'
   }
 };
 
+// Fake a wrapper node.
 var node = new HTMLElement('div');
 window.app = node;
 node.id = 'app';
@@ -19,9 +21,11 @@ node.nodeType = ELEMENT_NODE_TYPE;
 node.ownerDocument = document;
 node.firstChild = window.canvas;
 
+// Otherwise the canvas does not show up.
 window.canvas.style.width = window.innerWidth + 'px';
 window.canvas.style.height = window.innerHeight + 'px';
 
+// React uses those to compare with server rendering.
 window.canvas.getAttribute = function(attr){
   return this[attr];
 };
@@ -32,14 +36,42 @@ window.canvas.setAttribute = function(attr, value){
   this[attr] = value;
 };
 
+// Helps with hit test.
+window.canvas.getBoundingClientRect = function() {
+  return {
+    top: this.offsetTop, left: this.offsetLeft,
+    width: this.offsetWidth, height: this.offsetHeight,
+    right: this.offsetWidth, bottom: this.offsetHeight
+  };
+};
+
+// Better logging.
 window.console._log = function(level, args) {
   var txt = level + ':';
   for (var i = 0; i < args.length; i++) {
-    txt += ' ' + (typeof args[i] !== 'object' ? args[i].toString() : JSON.stringify(args[i]));
+    var arg = '';
+    if (Object.prototype.toString.call(args[i]) === "[object Object]") {
+      var seen = [];
+      arg = JSON.stringify(args[i], function(key, val) {
+        if (val != null && typeof val == "object") {
+          if (seen.indexOf(val) >= 0) return;
+          seen.push(val)
+        }
+        return val
+      });
+    }
+    else if (typeof args[i] == 'function') {
+      arg = args[i].toString();
+    }
+    else {
+      arg = args[i];
+    }
+    txt += ' ' + arg;
   }
-  ej.log( txt );
+  window.ejecta.log( txt );
 };
 
+// Fake a server rendering so that window.canvas will be reused by React.
 EjectaReactShim = function(outerHTML) {
   window.canvas['data-reactid'] = outerHTML.match(/ data-reactid="([^"]*)"/)[1];
   window.canvas['data-react-checksum'] = outerHTML.match(/ data-react-checksum="([^"]*)"/)[1];
