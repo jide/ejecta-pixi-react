db064d2d87b2a33179444fef0f8c00198139ac51
Julien De Luca <jide.fr@gmail.com>
2015-03-09 16:58:04 +0100

    A more generic approach

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,48 +1,22 @@
-var React = require('react/addons');
+var React = require('react');
 var ReactPIXI = require('react-pixi');
-
-if (typeof ejecta !== 'undefined') {
-  ReactPIXI.Stage.type.prototype.getDOMNode = function() {
-    return window.canvas;
-  }
-};
 
 var Stage = ReactPIXI.Stage;
 var Sprite = ReactPIXI.Sprite;
 var Text = ReactPIXI.Text;
 
 var App = React.createClass({
-  _raf: null,
-
-  addRandomSprite: function(event) {
-    console.log(event);
-  },
-
-  getInitialState: function() {
-    return {
-      x: 0
-    };
-  },
-
-  componentDidMount: function() {
-    this.tick();
-  },
-
-  tick: function() {
-    //console.log('tick');
-    this.setState({x: this.state.x + 1});
-    this._tick = requestAnimationFrame(this.tick);
-  },
-
   render: function() {
-    var fontstyle = {font:'40px TimesNewRomanPSMT'};
-    return <Stage width={this.props.width} height={this.props.height}>
-      <Sprite x={ this.state.x } interactive={ true } click={ this.addRandomSprite } image={ 'images/raspberry.png' } width={this.props.width} height={this.props.height} />
-      <Text text="Vector text" x={10} y={10} style={fontstyle} />
+    var fontstyle = {font:'40px Times'};
+    return <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Sprite image={'images/raspberry.png'} width={200} height={200} />
+      <Text text="Vector text" x={0} y={10} style={fontstyle} />
     </Stage>;
   }
 });
 
-var node = document.getElementById('app');
+if (typeof EjectaReactShim !== 'undefined') {
+  EjectaReactShim(React.renderToString(<App />));
+};
 
-React.render(<App width={window.innerWidth} height={window.innerHeight} />, node || document.body);
+React.render(<App />, window.app);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+// Shim the DOM for React.
+
 var ELEMENT_NODE_TYPE = 1;
 var DOC_NODE_TYPE = 9;
 
@@ -10,7 +12,29 @@ document.documentElement = {
   }
 };
 
-document.getElementById('canvas').style.width = window.innerWidth + 'px';
-document.getElementById('canvas').style.height = window.innerHeight + 'px';
+var node = new HTMLElement('div');
+window.app = node;
+node.id = 'app';
+node.nodeType = ELEMENT_NODE_TYPE;
+node.ownerDocument = document;
+node.firstChild = window.canvas;
+
+window.canvas.style.width = window.innerWidth + 'px';
+window.canvas.style.height = window.innerHeight + 'px';
+
+window.canvas.getAttribute = function(attr){
+  return this[attr];
+};
+window.canvas.removeAttribute = function(attr){
+  delete this[attr];
+};
+window.canvas.setAttribute = function(attr, value){
+  this[attr] = value;
+};
+
+EjectaReactShim = function(outerHTML) {
+  window.canvas['data-reactid'] = outerHTML.match(/ data-reactid="([^"]*)"/)[1];
+  window.canvas['data-react-checksum'] = outerHTML.match(/ data-react-checksum="([^"]*)"/)[1];
+}
 
 ejecta.include('app.js');
